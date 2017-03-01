c601a6481350ba845fc33609f11f6c687850bb08
Jack <jacklai5505@gmail.com>
2017-03-01 21:43:02 +0800

    Show no record description when no data saved.

--- app/src/main/java/tw/ctl/interest/history/HistoryFragment.java
+++ app/src/main/java/tw/ctl/interest/history/HistoryFragment.java
@@ -3,12 +3,12 @@ package tw.ctl.interest.history;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -26,6 +26,7 @@ import tw.ctl.interest.R;
 public class HistoryFragment extends Fragment {
 
     @BindView(R.id.recycler_view) RecyclerView recyclerView;
+    @BindView(R.id.description)   TextView description;
 
     private HistoryAdapter adapter;
 
@@ -52,12 +53,19 @@ public class HistoryFragment extends Fragment {
     private void fetchData() {
         Realm realm = Realm.getDefaultInstance();
         RealmResults<Entity> entities = realm.where(Entity.class).findAllSorted("date", Sort.DESCENDING);
+
+        recyclerView.setVisibility(entities.size() == 0 ? View.GONE : View.VISIBLE);
+        description.setVisibility(entities.size() == 0 ? View.VISIBLE : View.GONE);
+
         entities.addChangeListener(new RealmChangeListener<RealmResults<Entity>>() {
             @Override
-            public void onChange(RealmResults<Entity> element) {
+            public void onChange(RealmResults<Entity> elements) {
+                recyclerView.setVisibility(elements.size() == 0 ? View.GONE : View.VISIBLE);
+                description.setVisibility(elements.size() == 0 ? View.VISIBLE : View.GONE);
                 adapter.notifyDataSetChanged();
             }
         });
+
         adapter.setEntities(entities);
     }
 
