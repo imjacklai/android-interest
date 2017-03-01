7dca14c9153cdad6527fd3a1260f9f9175b8cc7b
Jack <jacklai5505@gmail.com>
2017-03-01 14:52:22 +0800

    Build structure.

---
+++ app/src/main/java/tw/ctl/interest/InfoFragment.java
@@ -0,0 +1,26 @@
+package tw.ctl.interest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by jacklai on 2017/3/1.
+ */
+
+public class InfoFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_info, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+}
