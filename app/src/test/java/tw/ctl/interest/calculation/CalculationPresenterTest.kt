fbe3325e6d044167d8511264023102f46d0eeae7
Jack <imjacklai@gmail.com>
2017-08-13 17:02:56 +0800

    Add unit test.

--- app/src/test/java/tw/ctl/interest/calculation/CalculationPresenterTest.kt
+++ app/src/test/java/tw/ctl/interest/calculation/CalculationPresenterTest.kt
@@ -10,10 +10,6 @@ import tw.ctl.interest.Entity
 
 import org.mockito.Mockito.verify
 
-/**
- * Created by jacklai on 2017/3/1.
- */
-
 class CalculationPresenterTest {
 
     @Mock
@@ -37,18 +33,26 @@ class CalculationPresenterTest {
 
     @Test
     @Throws(Exception::class)
-    fun calculate() {
-        val entity = Entity()
-        entity.principal = "1000"
-        entity.interest = "10"
-        entity.period = "10"
-        entity.invest = "1000"
-
+    fun calculateWithInvest() {
+        val entity = Entity("1000", "10", "10", "1000")
         presenter!!.calculate(entity)
 
         entity.simpleResult = "2,000.00"
         entity.compoundResult = "2,593.74"
         entity.investResult = "18,531.17"
+
+        verify<CalculationView>(view).onResult(entity)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun calculateWithoutInvest() {
+        val entity = Entity("1000", "10", "10", "")
+        presenter!!.calculate(entity)
+
+        entity.simpleResult = "2,000.00"
+        entity.compoundResult = "2,593.74"
+        entity.investResult = "---"
 
         verify<CalculationView>(view).onResult(entity)
     }
