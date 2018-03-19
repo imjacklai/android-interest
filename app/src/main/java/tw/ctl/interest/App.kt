ae23611805a0f68de0d9b28021141727ce944ce1
Jack <imjacklai@gmail.com>
2018-03-19 14:24:37 +0800

    Integrate LeakCanary.

--- app/src/main/java/tw/ctl/interest/App.kt
+++ app/src/main/java/tw/ctl/interest/App.kt
@@ -1,9 +1,12 @@
 package tw.ctl.interest
 
 import android.app.Application
+import com.squareup.leakcanary.LeakCanary
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        if (LeakCanary.isInAnalyzerProcess(this)) return
+        LeakCanary.install(this)
     }
 }
